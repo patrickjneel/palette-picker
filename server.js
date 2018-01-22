8873201aa147b22d18d864f85a0fa7a884d77253
Pat Neel <patrick.neel@colorado.edu>
2018-01-22 14:14:54 -0700

    added line to display static-content

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 
 
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.locals.title = 'Palette-Picker';
-
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is listening at ${app.get('port')}`)
