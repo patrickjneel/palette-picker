daccb6f3a7224e4c99a27edd174418b3254795d7
Pat Neel <patrick.neel@colorado.edu>
2018-01-23 16:47:13 -0700

    lock functionality is working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,41 +1,34 @@
-
+$(document).ready(() => {
+  allColors()
+});
 
 const generateColor = () => {
-  var length = 6;
-  var chars = '0123456789ABCDEF';
-  var hex = '#';
+
+  let length = 6;
+  let chars = '0123456789ABCDEF';
+  let hex = '#';
   while(length--) 
     hex += chars[Math.floor(Math.random() * 16)];
   return hex;
 }
 
 const allColors = () => {
-  const color1 = generateColor()
-  const color2 = generateColor()
-  const color3 = generateColor()
-  const color4 = generateColor()
-  const color5 = generateColor()
-
-  $("#color1").css("background-color", color1)
-  $(".hex1").text(color1);
-  $("#color2").css("background-color", color2)
-  $(".hex2").text(color2);
-  $("#color3").css("background-color", color3)
-  $(".hex3").text(color3);
-  $("#color4").css("background-color", color4)
-  $(".hex4").text(color4);
-  $("#color5").css("background-color", color5)
-  $(".hex5").text(color5);
+  const colors = $('.color')
+  colors.each(function() {
+    if(!$(this).hasClass('selected')) {
+      let color = generateColor()
+      $(this).css("background-color", color)
+      $(this).find('.hex-value').text(color) 
+    }
+  }) 
 }
 
 const lockColor = (event) => {
-  $(event.target).toggleClass('locked').css("display", "flex")
+   $(event.target).toggleClass('locked').css("display", "flex");
+   $(event.target).parent('.color').toggleClass('selected')
 }
 
 $("#generate-btn").on('click', allColors);
 $(".unlocked").on('click', (event) => lockColor(event));
 
-$(document).ready(() => {
-  allColors()
-});
 
