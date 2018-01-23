ebb324dd21ab6b739143f291369aaf11295799aa
Pat Neel <patrick.neel@colorado.edu>
2018-01-22 20:18:41 -0700

    added to sort of generate the hex colors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,20 @@
 const generateColor = () => {
   var length = 6;
-  var chars = '0123456789ABCDEF'
+  var chars = '0123456789ABCDEF';
   var hex = '#';
   while(length--) hex += chars[(Math.random() * 16) | 0];
-  
+  console.log(hex)
   return hex;
 }
 
 
 
-$("#generate-btn").on('click', generateColor);
+$("#generate-btn").on('click', () => {
+  $(".color").css("background-color", generateColor())
+});
 
-
+$("#color1").css("background-color", generateColor())
+$("#color2").css("background-color", generateColor())
+$("#color3").css("background-color", generateColor())
+$("#color4").css("background-color", generateColor())
+$("#color5").css("background-color", generateColor())
