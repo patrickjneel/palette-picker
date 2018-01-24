a180bc2522d1e2c21a063764af5d9789036e3205
Pat Neel <patrick.neel@colorado.edu>
2018-01-24 15:18:44 -0700

    hooked up database to the server

---
+++ db/seeds/dev/projects.js
@@ -0,0 +1,31 @@
+
+exports.seed = function(knex, Promise) {
+  
+  return knex('palette').del()
+    .then(() => knex('projects').del())
+
+    .then(function () {
+
+      return Promise.all([
+        knex('projects').insert({
+          projectName: 'Project1'
+        }, 'id')
+        .then(palettes => {
+          return knex('palette').insert([
+            {
+              projectName: 'Project1', 
+              paletteName: 'Shit', 
+              color1: '#FF', 
+              color2: '#FF',
+              color3: '#FF', 
+              color4: '#FF', 
+              color5: '#FF',
+              projects_id: palettes[0]
+            }
+          ])
+        })
+        .then(() => console.log('seeding complete'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ]) //end Promise.all
+    });
+};
